f6e0bbdc1e30f061a11817f5022fde72f69fa8aa
TanishqAhuja <tanishq.ahuja@mountblue.io>
2019-10-24 12:09:29 +0530

    final

--- src/Story.js
+++ src/Story.js
@@ -4,9 +4,14 @@ import { Link } from 'react-router-dom';
 class Story extends React.Component {
     state = {
         score: this.props.story['score'],
-        style: {
+        totalComments: this.props.story['kids'].length,
+        style1: {
             "textDecoration": "none",
             "color": "black",
+        },
+        style2: {
+            "textDecoration": "none",
+            "color": "blue",
         }
     }
 
@@ -18,6 +23,7 @@ class Story extends React.Component {
         this.setState({ score: this.state.score - 1 });
     };
 
+
     render() {
         const commentsUrl = `/comments/${this.props.story['item_id']}`;
         return (
@@ -25,8 +31,8 @@ class Story extends React.Component {
                 <button className="border-0 p-0 bg-white text-success" onClick={this.upVotes}>▲</button>
                 <button className="border-0 ml-1 p-0 bg-white text-danger" onClick={this.downVotes}>▼</button>
                 <span className="mr-auto ml-3 text-warning">{this.state.score}</span>
-                <span className="mr-auto"><a style={this.state.style} href={this.props.story['url']}>{this.props.story['title']}</a></span>
-                <button className="text-info p-0 bg-white border-0"><Link to={commentsUrl}>{this.props.story['kids'].length}  comments</Link></button>
+                <span className="mr-auto"><a style={this.state.style1} href={this.props.story['url']}>{this.props.story['title']}</a></span>
+                <button className="text-info p-0 bg-white border-0"><Link to={commentsUrl} style={this.state.style2}>{this.state.totalComments}  comments</Link></button>
             </div >
         );
     }
